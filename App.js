7974841b425299a5d5475633a27f9927bfe15490
Erick Burgos <eburgos@takhyon.com>
2021-07-12 04:52:07 -0600

    Primer push, me faltan componentes aún

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import { Map, Modal, Panel} from './components';
+
 
 export default function App() {
+  const handlersLongPress =({nativeEvent})=>{
+    console.log(nativeEvent);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Map longPress={handlersLongPress} />
+        <Modal/>
+        <Panel/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
+ 
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
